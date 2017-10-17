ec64893b2efe0d799ce9a95bcd20e9d002ead423
TatsuhiroAikawa <tacho119@gmail.com>
2017-10-17 11:18:57 +0000

    new_toukou

--- practice_production/app/controllers/posts_controller.rb
+++ practice_production/app/controllers/posts_controller.rb
@@ -1,10 +1,17 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
   
   def new
   end
   
   def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    
+    #ひとまずindexに飛ばしておく
+    redirect_to("/posts/index")
   end
+  
 end
